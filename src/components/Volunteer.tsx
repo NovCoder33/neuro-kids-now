cbba1afee8baaa947164bc3a52d9baa3d697355d
SidR <68519263+SidCoderMan@users.noreply.github.com>
2025-07-09 18:24:56 -0500

    Updated CSS for Volunteer page, changed background color for Program cards.

--- src/components/Volunteer.tsx
+++ src/components/Volunteer.tsx
@@ -3,36 +3,38 @@ import "./Volunteer.css";
 const Volunteer = () => {
   return (
     <div className="volunteer">
-      <h1>Get Involved</h1>
-      <div className="button">
-        <button
-          onClick={() =>
-            window.open(
-              "https://docs.google.com/forms/d/1hHw6GMFzMKns828143kUZ4J36J9rAR6TN7Z8nqKhze8/edit"
-            )
-          }
-        >
-          Become a Volunteer
-        </button>
-        <button
-          onClick={() =>
-            window.open(
-              "https://docs.google.com/forms/d/1JdMGGFqd6vru-175CdLMwohuPDl1yGdlpmySoJONhnw/edit#response=ACYDBNgarqa6BY6YDonmy1QSm_D2OFGg_hI0c2-F8RqXhSfrmCIvBSYs-va_MVX1ITVLeQw"
-            )
-          }
-        >
-          Start a Chapter
-        </button>
-        <button
-          onClick={() =>
-            window.open(
-              "https://docs.google.com/forms/d/e/1FAIpQLSfYHZke8QmIHSF6lBscPLU6fktXU7c9I1CpuikKabM8WLohww/viewform?usp=header"
-            )
-          }
-        >
-          Be a Leader
-        </button>
-      </div>
+      <header className="header">
+        <h1>Get Involved</h1>
+        <div className="button">
+          <button
+            onClick={() =>
+              window.open(
+                "https://docs.google.com/forms/d/1hHw6GMFzMKns828143kUZ4J36J9rAR6TN7Z8nqKhze8/edit"
+              )
+            }
+          >
+            Become a Volunteer
+          </button>
+          <button
+            onClick={() =>
+              window.open(
+                "https://docs.google.com/forms/d/1JdMGGFqd6vru-175CdLMwohuPDl1yGdlpmySoJONhnw/edit#response=ACYDBNgarqa6BY6YDonmy1QSm_D2OFGg_hI0c2-F8RqXhSfrmCIvBSYs-va_MVX1ITVLeQw"
+              )
+            }
+          >
+            Start a Chapter
+          </button>
+          <button
+            onClick={() =>
+              window.open(
+                "https://docs.google.com/forms/d/e/1FAIpQLSfYHZke8QmIHSF6lBscPLU6fktXU7c9I1CpuikKabM8WLohww/viewform?usp=header"
+              )
+            }
+          >
+            Be a Leader
+          </button>
+        </div>
+      </header>
       <div className="description">
         <div className="descItem">
           <h1> Become a Volunteer </h1>
