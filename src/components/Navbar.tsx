08143011711bee2e69d21b920511ba252f06a2f2
SidR <68519263+SidCoderMan@users.noreply.github.com>
2025-07-12 12:15:15 -0500

    Updated navbar according to design plan.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,21 +5,40 @@ const Navbar = () => {
   const navigate = useNavigate();
   return (
     <nav className="navbar">
+      <hr></hr>
       <ul>
         <li>
-          <Link to="./">Contact</Link>
+          <Link style={{ fontFamily: "Raleway" }} to="./">
+            CONTACT
+          </Link>
         </li>
+        <div className="vertical"></div>
         <li>
-          <Link to="./Programs">Programs</Link>
+          <Link style={{ fontFamily: "Raleway" }} to="./Volunteer">
+            GET INVOLVED
+          </Link>
         </li>
+        <div className="vertical"></div>
         <li>
-          <Link to="./Volunteer">Volunteer</Link>
+          <Link style={{ fontFamily: "Raleway" }} to="./WhoWeAre">
+            OUR IMPACT
+          </Link>
         </li>
+        <div className="vertical"></div>
         <li>
-          <Link to="./WhoWeAre">Who We Are</Link>
+          <Link style={{ fontFamily: "Raleway" }} to="./Programs">
+            PROGRAMS
+          </Link>
+        </li>
+        <div className="vertical"></div>
+        <li>
+          <Link style={{ fontFamily: "Raleway" }} to="./WhoWeAre">
+            ABOUT
+          </Link>
         </li>
         <img onClick={() => navigate(`./`)} src={logo} alt="logo-img" />
       </ul>
+      <hr></hr>
     </nav>
   );
 };
