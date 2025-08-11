25c046d64e3c6b5ba941e381d7c42cf466d7051b
SidR <68519263+SidCoderMan@users.noreply.github.com>
2025-08-11 13:45:08 -0500

    Updated volunteer and our programs page with the new design plans

--- src/components/Programs.tsx
+++ src/components/Programs.tsx
@@ -1,82 +1,48 @@
 import "./Programs.css";
-import toyImage from "../assets/toyscrafts.jpg";
-import cardsCraft from "../assets/cardscrafts.jpg";
-import interactions from "../assets/hospitalinteractions.jpg";
-import carePackage from "../assets/carepackages.jpeg";
-import penPal from "../assets/patientpenpal.jpeg";
+import img1 from "../assets/img.png";
+import img2 from "../assets/img2.png";
+import img3 from "../assets/img4.png";
+import img4 from "../assets/img5.png";
+import img5 from "../assets/hospital.jpg";
+
 const Programs = () => {
   return (
     <div className="programs">
       <h1>Our Programs</h1>
-      <div className="programTile">
-        <div className="programText">
-          <h1>Toys & Books</h1>
-          <p>
-            We collect and donate brand-new, non-violent, non-latex toys, books,
-            and board games to pediatric neurology units at partner hospitals
-            like Cincinnati Children’s. These items are carefully selected to
-            offer cognitive stimulation, sensory engagement, and emotional
-            comfort to kids battling neurological conditions. We prioritize
-            educational materials that can be enjoyed from hospital beds or
-            therapy rooms.
-          </p>
+      <div className="program-hero">
+        <div className="program-sec">
+          <button>Toys/Books</button>
+          <img src={img1}></img>
         </div>
-        <img src={toyImage} alt="toybooksprogramimg" />
+        <div className="program-sec">
+          <button>Cards/Crafts</button>
+          <img src={img2}></img>
+        </div>
+        <div className="program-sec">
+          <button>Care Packages</button>
+          <img src={img3}></img>
+        </div>
+        <div className="program-sec">
+          <button>Patient Pen Pal</button>
+          <img src={img4}></img>
+        </div>
+        <div className="program-sec">
+          <button>Hospital Recources</button>
+          <img src={img5}></img>
+        </div>
       </div>
-      <div className="programTile">
-        <div className="programText">
-          <h1>Cards & Crafts</h1>
-          <p>
-            Volunteers of all ages create uplifting handmade cards, digital
-            e-cards, and simple craft kits that are delivered to children in
-            neurology wards. Every message is reviewed to ensure it’s
-            trauma-informed, cheerful, and inclusive. These cards are
-            distributed during holidays, awareness months, or alongside care
-            packages to offer a personal touch and brighten a child’s day.
-          </p>
-        </div>
-        <img src={cardsCraft} alt="cardscraftsimg" />
-      </div>
-      <div className="programTile">
-        <div className="programText">
-          <h1>Patient Pen Pal</h1>
-          <p>
-            This program allows volunteers to connect with children in hospitals
-            through written letters and pre-recorded video messages. These
-            interactions are designed to ease isolation, celebrate milestones,
-            and offer friendship. Volunteers are trained to be age-appropriate,
-            empathetic, and sensitive to patients’ medical conditions and
-            privacy.
-          </p>
-        </div>
-        <img src={penPal} alt="interactionsimg" />
-      </div>
-      <div className="programTile">
-        <div className="programText">
-          <h1>Care Packages</h1>
-          <p>
-            We deliver customized care packages to children undergoing long-term
-            treatments. Each package includes a thoughtful mix of sensory toys,
-            books, cozy items like socks or fidget tool(s), and handmade notes.
-            When possible, packages are tailored to a child’s age, diagnosis,
-            and interests, based on information provided by hospital partners or
-            families.
-          </p>
-        </div>
-        <img src={carePackage} alt="carepackageimg" />
-      </div>
-      <div className="programTile">
-        <div className="programText">
-          <h1>Hospital Resources</h1>
-          <p>
-            We identify and support under-resourced pediatric neurology units by
-            assessing and donating essential medical supplies, therapy
-            equipment, and accessibility tools. This may include EEG caps or
-            adaptive seating tools. We collaborate with hospital staff to ensure
-            every item meets regulatory standards and local needs.
-          </p>
-        </div>
-        <img src={interactions} alt="hospitalimg" />
+      <div className="gold-body-programs">
+        <p>
+          At Neuro Kids Now, we created these programs to support kids with
+          neurological conditions who often face long hospital stays, isolation,
+          and overwhelming symptoms. Many of these children are neurodivergent
+          and experience challenges like anxiety, sensory sensitivity, and
+          communication barriers. Through care packages, letters, crafts, and
+          community support, our programs are designed to bring comfort,
+          connection, and joy to kids who may feel forgotten in the hospital
+          system. We believe every child deserves to feel seen, supported, and
+          celebrated—especially during tough times!
+        </p>
       </div>
     </div>
   );
--- src/components/Volunteer.tsx
+++ src/components/Volunteer.tsx
@@ -1,87 +1,50 @@
 import "./Volunteer.css";
-import toyImage from "../assets/toyscrafts.jpg";
+import img1 from "../assets/img1.png";
+import img2 from "../assets/hero.png";
+import img3 from "../assets/img3.jpg";
 
 const Volunteer = () => {
   return (
-    <div>
-      <div className="volunteer-hero">
-        <h1>Get Involved</h1>
-        <div className="volunteer-hero-content">
-          <div className="volunteer-hero-card">
-            <img src={toyImage} alt="placeholder"></img>
-            <button
-              onClick={() =>
-                window.open(
-                  "https://docs.google.com/forms/d/1hHw6GMFzMKns828143kUZ4J36J9rAR6TN7Z8nqKhze8/edit"
-                )
-              }
-            >
-              Become a Volunteer
-            </button>
-          </div>
-          <div className="volunteer-hero-card">
-            <img src={toyImage} alt="placeholder"></img>
-            <button
-              onClick={() =>
-                window.open(
-                  "https://docs.google.com/forms/d/1JdMGGFqd6vru-175CdLMwohuPDl1yGdlpmySoJONhnw/edit#response=ACYDBNgarqa6BY6YDonmy1QSm_D2OFGg_hI0c2-F8RqXhSfrmCIvBSYs-va_MVX1ITVLeQw"
-                )
-              }
-            >
-              Start a Chapter
-            </button>
-          </div>
-          <div className="volunteer-hero-card">
-            <img src={toyImage} alt="placeholder"></img>
-            <button
-              onClick={() =>
-                window.open(
-                  "https://docs.google.com/forms/d/e/1FAIpQLSfYHZke8QmIHSF6lBscPLU6fktXU7c9I1CpuikKabM8WLohww/viewform?usp=header"
-                )
-              }
-            >
-              Be a Leader
-            </button>
-          </div>
+    <div className="volunteer">
+      <h1> Get Involved</h1>
+      <div className="v-hero">
+        <div className="v-sec">
+          <button>START A CHAPTER</button>
+          <img src={img1}></img>
+        </div>
+        <div className="v-sec">
+          <button>BECOME A VOLUNTEER</button>
+          <img src={img2}></img>
+        </div>
+        <div className="v-sec">
+          <button>LEADERSHIP TEAM</button>
+          <img src={img3}></img>
         </div>
       </div>
-      <div className="volunteer-content">
-        <div className="volunteer-content-card">
-          <h1> Become a Volunteer </h1>
-          <p>
-            At NeuroKidsNow, our volunteers are the heart of everything we do.
-            By becoming a volunteer, you’ll help create meaningful experiences
-            for children with neurological disabilities — whether that’s through
-            mentoring, organizing events, or simply being a supportive presence.
-            Your time and compassion can help build confidence, joy, and
-            opportunities for kids who need it most. Join us and see how your
-            small actions can create lasting change in their lives.
-          </p>
-        </div>
-        <div className="volunteer-content-card">
-          <h1> Start a Chapter </h1>
-          <p>
-            You can help bring NeuroKidsNow to your own community by starting a
-            chapter! We’ll guide you every step of the way as you establish a
-            local hub of support and resources for children with neurological
-            disabilities and their families. A chapter allows you to organize
-            events, foster connections, and spread awareness in a way that’s
-            tailored to the unique needs of your area. By starting a chapter,
-            you become a catalyst for inclusion and hope right where you live.
-          </p>
-        </div>
-        <div className="volunteer-content-card">
-          <h1> Be a Leader </h1>
-          <p>
-            At NeuroKidsNow, we believe leadership comes from passion and
-            purpose. When you step up to be a leader in our organization, you
-            have the chance to inspire others, shape programs, and advocate for
-            kids with neurological disabilities. Leaders help us grow our
-            mission by organizing teams, speaking up for inclusivity, and
-            driving new initiatives that open doors for children who face unique
-            challenges. Use your voice and your vision to help guide the way
-            forward.
-          </p>
+      <div className="gold-body">
+        <p>
+          Want to make a real impact? Neuro Kids Now is powered by passionate
+          teens and young adults who care about kids with neurological
+          conditions. You can get involved by starting a chapter in your
+          community, joining our leadership team, or volunteering for one of our
+          programs—whether that’s packing care packages, running donation
+          drives, or helping spread awareness. We make it easy to get started,
+          and you’ll get service hours, leadership experience, and the chance to
+          be part of something that truly matters.
+        </p>
+        <div className="gold-body-text">
+          <h1>Want to see real examples of youth-led impact in action?</h1>
+          <h1>
+            Check out our{" "}
+            <a
+              href="https://www.instagram.com/neurokidsnow_"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+            !
+          </h1>
         </div>
       </div>
     </div>
